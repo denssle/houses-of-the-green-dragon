d7f3d4f65e70ef2314bfbc8aa83674410c125b8c
Dominik Hellweg <dominik.hellweg@cdi-ag.de>
2025-03-18 18:01:29 +0100

    add first tailwind

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,17 +4,21 @@
 	let { data, children } = $props();
 </script>
 
-<header>
+<header class="flex place-content-around">
 	{#each data.sections as section}
-		<a href="/{section.slug}">{section.title}</a>
+		<a href="/{section.slug}" class="text-blue-600 visited:text-purple-600">{section.title}</a>
 	{/each}
 </header>
 
 {@render children()}
 
 <footer>
-	<nav>
-		<a href="/about">Über die Seite</a>
-		<a href="/impressum">Impressum</a>
+	<nav class="flex place-content-around">
+		<a href="/about" class="text-blue-600 visited:text-purple-600">
+			Über die Seite
+		</a>
+		<a href="/impressum" class="text-blue-600 visited:text-purple-600">
+			Impressum
+		</a>
 	</nav>
 </footer>
